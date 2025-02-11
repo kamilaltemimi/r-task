80d5e768d2eaf8a7852c61b2f26d9ca6d6607f20
kamilaltemimi <kamilaltemimi@gmail.com>
2025-02-11 13:53:13 +0100

    feat: unsubscribe stream on destroy

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { CrewService } from '../../core/services/crew/crew.service';
-import { ActivatedRoute } from '@angular/router';
+
 import { CrewMember } from '@Core/interfaces/crew-member.interface';
+
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-profile-page',
@@ -11,6 +14,8 @@ import { CrewMember } from '@Core/interfaces/crew-member.interface';
 })
 export class ProfilePageComponent implements OnInit {
   selectedCrewMember = signal<CrewMember | null>(null);
+
+  private destroy$ = new Subject<void>();
 
   constructor(
     private crewService: CrewService,
@@ -21,18 +26,26 @@ export class ProfilePageComponent implements OnInit {
     this.getCrewMemberDetails();
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   getCrewMemberDetails(): void {
     const crewMemberId = this.activatedRoute.snapshot.params['id'];
 
-    this.crewService.getCrewData().subscribe((crewMembers) => {
-      const foundCrewMember = crewMembers.find(
-        (member) => member.id === crewMemberId
-      );
-      if (foundCrewMember) {
-        this.selectedCrewMember.set(foundCrewMember);
-      } else {
-        console.error(`Crew member with ID ${crewMemberId} not found.`);
-      }
-    });
+    this.crewService
+      .getCrewData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((crewMembers) => {
+        const foundCrewMember = crewMembers.find(
+          (member) => member.id === crewMemberId
+        );
+        if (foundCrewMember) {
+          this.selectedCrewMember.set(foundCrewMember);
+        } else {
+          console.error(`Crew member with ID ${crewMemberId} not found.`);
+        }
+      });
   }
 }
